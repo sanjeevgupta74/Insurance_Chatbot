c4cb06883e4d4c2a38ed2da921a843e6703767c7
sanjeevgupta74 <sanjeev_gupta74@yahoo.com>
2025-04-05 16:12:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ if user_input:
         with st.chat_message("assistant"):
             st.markdown(assistant_response)
   #    else:
-else
+else:
 # Reject off-topic queries
   rejection_message = "This chat is strictly about health tips. Please ask health-related questions."
   st.session_state.messages.append({"role": "assistant", "content": rejection_message})
